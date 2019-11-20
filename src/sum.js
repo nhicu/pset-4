abbd7eb116b684ec5d652276fc3313159b329484
nhicu <55456980+nhicu@users.noreply.github.com>
2019-11-20 12:53:16 -0500

    merging local and remote repos

--- src/sum.js
+++ src/sum.js
@@ -1,13 +1,23 @@
 const readlineSync = require("readline-sync");
 
+const MIN = Number.MIN_SAFE_INTEGER
+const MAX = Number.MAX_SAFE_INTEGER
 
 let lowerb = -1;
 let upperb = 1;
 
+console.log();
 do {
 lowerb = Number(readlineSync.question("Lower bound: "));
 upperb = Number(readlineSync.question("Upper bound: "));
-} while (lowerb>>upperb || Number.isNaN(Lowerb) || Number.isNaN(Upperb));
+} while (lowerb>>upperb || Number.isNaN(lowerb) || Number.isNaN(upperb));
 
 let sum = 0;
-for(let i = lowerb; i<=upperbound; )
+
+for (let i = lowerb; i <= upperb; i++) {
+
+  if (i % 2 === 0) {
+    sum = (sum + i);
+    }
+}
+  console.log(`\n${sum.toLocaleString()}.`);
